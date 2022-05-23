dfbe07845bfb6671b27b99c6a5ce7d76369eafcc
Kane696 <kanekonatesangare@gmail.com>
2022-05-23 16:27:06 +0200

    ajout de la fonctionnalité: fermeture de la modal

--- starterOnly/js/modal.js
+++ starterOnly/js/modal.js
@@ -11,13 +11,30 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const closeBtn = document.querySelector(".close");
+const modalContent = document.querySelector(".content");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
+// close modal event
+closeBtn.addEventListener("click", closeModal);
+
+
 // launch modal form
 function launchModal() {
+  modalContent.style.animationName = "modalopen";
+  modalContent.style['-webkit-animation-duration'] = ".8s";
   modalbg.style.display = "block";
 }
 
+// close modal form
+function closeModal() {
+    modalContent.style.animationName = "modalclose"
+    modalContent.style['-webkit-animation-duration'] = "1s";
 
+  setTimeout(function(){
+    modalbg.style.display = "none";
+  }, 900);
+  
+}
